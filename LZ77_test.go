cde08425e2507ef1384ab6171b399bbda2033bf5
Urko López <urkolopezlopez@gmail.com>
2026-05-31 18:35:07 +0200

    Eliminar logs innecesarios

--- LZ77_test.go
+++ LZ77_test.go
@@ -1,21 +1,17 @@
 package lz77
 
 import (
-	"fmt"
 	"testing"
 	"os"
-
-	"github.com/urkx/lgo"
 )
 
+const lorem string = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec quis libero ac arcu sodales laoreet quis vitae enim. Sed placerat arcu nec lorem consequat pellentesque. Cras accumsan velit sed fermentum suscipit. Pellentesque efficitur libero eu faucibus pulvinar. Curabitur aliquam libero fermentum tempor posuere. Cras euismod odio ac sagittis ornare. Aliquam vel venenatis neque. Donec at posuere justo. Donec iaculis orci eu hendrerit dignissim. Nulla facilisi. Praesent ac purus quis nisi facilisis posuere. Curabitur at urna vitae justo laoreet ornare nec vel tellus. Curabitur accumsan justo pulvinar, aliquet odio ut, auctor metus. Mauris lectus libero, malesuada sed sem non, egestas rhoncus urna. Aenean ipsum mauris, commodo in suscipit sit amet, tincidunt non sapien. Aenean a leo tortor.Nullam quis erat quam. Etiam a sem ut odio congue suscipit ut eget neque. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu vehicula purus, ut semper ligula. Mauris bibendum lacinia nisi sed volutpat. Quisque vel dolor elit. Nulla facilisi. Nullam nec ex ut odio elementum laoreet. Nulla ut ultrices velit, a gravida tortor. Mauris eget tristique tellus. Praesent mollis, odio non facilisis cursus, tellus ex eleifend nunc, et consectetur lectus nisl ac ipsum.Morbi mi nunc, viverra eu pretium non, rutrum ac magna. Donec interdum pulvinar est, eget luctus lacus commodo eu. Morbi tempor dignissim tellus, vel interdum lectus auctor eu. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nam accumsan vestibulum odio sed consequat. Mauris eu placerat nulla. Integer consectetur in justo quis lacinia. Fusce maximus purus enim, eget tempor odio vulputate sed. Aenean commodo massa massa, mattis vestibulum dui dapibus in. Pellentesque porta fringilla nunc, rhoncus imperdiet turpis lobortis in.Morbi lectus sem, tincidunt ut volutpat ac, congue eu nunc. Morbi feugiat odio sem, vel egestas orci dictum eget. Sed hendrerit massa sit amet posuere pharetra. Nunc eleifend gravida dui, eu congue nisi porttitor id. Pellentesque eu leo id massa placerat rhoncus. Vivamus euismod dui eu leo venenatis, ac viverra metus convallis. Curabitur convallis sollicitudin ante vel condimentum. Morbi non maximus ante. Sed dictum imperdiet gravida. Duis lectus justo, tristique a tempus laoreet, egestas ut nisi. In nisi enim, ornare sit amet ligula in, ullamcorper tempor purus.Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nulla egestas tellus quis leo fringilla lacinia. Nullam ut leo fringilla, fermentum massa vel, tempor enim. Etiam pretium mi at accumsan varius. Duis congue efficitur justo fringilla varius. In id erat suscipit, pellentesque nisl vel, vulputate nulla. Suspendisse potenti. Sed vel aliquam diam. Etiam mattis commodo odio, vel eleifend dui commodo sit amet. Quisque ac condimentum arcu. Donec sollicitudin vitae tortor id egestas. Mauris consectetur libero vitae euismod pulvinar. Quisque tempus porta interdum. Cras mollis congue sem, ac rhoncus est facilisis at. Praesent maximus est eu neque commodo, id egestas magna aliquet."
+
 func TestLz(t *testing.T) {
-	test := "tres tristes tigres tragaban trigo en un trigal"
-	c := Compress(test, 32000)
-	fmt.Print(lgo.Info("Compression finished"))
-	fmt.Print(lgo.Debug(c))
+	c := Compress(lorem, 32000)
 	res := Decompress(c)
 
-	if res != test {
+	if res != lorem {
 		t.Fatal("Compression failed")
 	}
 }
@@ -23,14 +19,11 @@ func TestLz(t *testing.T) {
 func TestWriteFile(t *testing.T) {
 	test := "tres tristes tigres tragaban trigo en un trigal"
 	c := Compress(test, 32000)
-	fmt.Print(lgo.Info("Compression finished"))
-	fmt.Print(lgo.Info("Creating file"))
 	err := WriteResultFile("test.lz77", c)
 
 	if err != nil {
 		t.Fatal("WriteFile failed")
 	}
-	fmt.Print(lgo.Info("File created successfully"))
 	os.Remove("test.lz77")
 }
 
@@ -45,13 +38,11 @@ func TestReadFile(t *testing.T) {
 	if error != nil || string(read) != test_content {
 		t.Fatal("Readed content is not equal to test content")
 	}
-	fmt.Print(lgo.Info("File readed successfully"))
 	os.Remove("test.input")
 }
 
 func TestResultFromBytes(t *testing.T) {
-	test := "tres tristes tigres tragaban trigo en un trigal"
-	c := Compress(test, 32000)
+	c := Compress(lorem, 32000)
 	
 	err := WriteResultFile("test.lz77", c)
 	if err != nil {
@@ -64,11 +55,8 @@ func TestResultFromBytes(t *testing.T) {
 	}
 
 	parsed := ParseBytes(read)
-	fmt.Print(lgo.Debug(c))
-	fmt.Print(lgo.Debug(parsed))
 	if len(parsed) != len(c) {
 		t.Fatal("Parsed not equal than compressed")
 	}
-	fmt.Print(lgo.Info("File parsed successfully"))
 	os.Remove("test.lz77")
 }
--- go.mod
+++ go.mod
@@ -1,5 +1,3 @@
 module github.com/urkx/LZ77
 
 go 1.22.5
-
-require github.com/urkx/lgo v0.1.3 // indirect
